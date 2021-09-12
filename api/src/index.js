a1d505829f6c487084ff487f0f5038edd5c2b7d9
rafaelladamiski <rafaelladamiski@gmail.com>
2021-09-11 21:15:18 -0300

    ok

--- api/src/index.js
+++ api/src/index.js
@@ -8,12 +8,63 @@ app.use(express.json());
 app.get('/matricula', async (req, resp) => {
     try {
         let alunos = await db.tb_matricula.findAll()
+        resp.send (alunos);
     } catch (e) {
-        resp.send( { erro: e.toString ()} )
+        resp.send( 'Algo de errado não está certo' )
     }
         
 })
 
+app.post('/matricula', async (req, resp) => {
+    try {
+        let { nome, chamada, curso, turma } = req.body;
+
+        let r = await db.tb_matricula.create({
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+        })
+        resp.send(r);
+
+    } catch (e) {
+        resp.send( 'Algo de errado não está certo' )
+    }
+})
+
+app.put('/matricula/:id', async (req, resp) => {
+    try {
+        let { nome, chamada, curso, turma } = req.body;
+        let { id } = req.params;
+
+        let r = await db.tb_matricula.update(
+            {
+                nm_aluno: nome,
+                nr_chamada: chamada,
+                nm_curso: curso,
+                nm_turma: turma
+            },
+
+            {
+                where: { id_matricula: id}
+            }
+        )
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send('Algo de errado não está certo')
+    }
+})
+
+app.delete('/matricula/:id', async (req, resp) => {
+    try{
+        let { id } = req.params;
+
+        let r = await db.tb_matricula.destroy({ where: { id_matricula: id } })
+        resp.sendStatus(200);
+    }catch (e) {
+        resp.send('Algo de errado não está certo')
+    }
+})
 
 app.listen(process.env.PORT,
 
